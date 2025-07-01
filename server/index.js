15f3df5c6afb03fc38b02ee576373b35a76f76bd
Sasindu Malhara <sasindumalhara@gmail.com>
2025-07-01 17:58:59 +0530

    Update PORT configuration in .env file from 5000 to 5001

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
